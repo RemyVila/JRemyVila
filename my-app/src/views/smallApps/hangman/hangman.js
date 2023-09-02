f2b64625b6e8bb368e241df871abd238e9825ca6
Remy Vila <josephvila123@gmail.com>
2023-09-02 00:53:15 -0400

    unblocked react testing env from django server, GUI mapping data from axios

--- my-app/src/views/smallApps/hangman/hangman.js
+++ my-app/src/views/smallApps/hangman/hangman.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Hangman() {
+  const [wordBankData, setWordBankData] = useState([]);
+  const testingAPI = 'http://127.0.0.1:8000/';
+
+  useEffect(() => {
+    // Make a GET request to the API endpoint
+    axios.get(`${testingAPI}api/hangman-wordbank/`)
+      .then((response) => {
+        // Handle the response data
+        setWordBankData(response.data);
+        console.log(wordBankData)
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error('Error fetching data:', error);
+      });
+  }, []); // The empty dependency array ensures this effect runs once on component mount
+
   return (
     <div>
       <div>
-        <h3>
-            Hangman
-        </h3>
-            Terminal based hangman.py integrated to a Django backend to
-            handle the operations and display via React.
-            
+        <h3>Hangman</h3>
+        {/* Render the fetched data */}
+        <div>
+          {wordBankData.map((item) => (
+            <li key={item.id}>
+              Word: {item.name}
+              Hint: {item.hint}
+            </li>
+          ))}
+        </div>
       </div>
-
     </div>
   );
 }
